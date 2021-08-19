bfd433b1ff2fcf49b354f0e7d856375e3f5feeea
exaon18 <69327021+exaon18@users.noreply.github.com>
2021-08-19 08:34:10 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,114 +1,324 @@
-#Do not change, change the script cache then error
-#Import
+
+
+Python code
 from telethon import TelegramClient, sync, events
-from telethon.tl.functions.messages import GetHistoryRequest, GetBotCallbackAnswerRequest
+from telethon.tl.functions.messages import (
+    GetHistoryRequest,
+    GetBotCallbackAnswerRequest,
+)
 from telethon.errors import SessionPasswordNeededError
-from bs4 import BeautifulSoup
+from telethon.errors import FloodWaitError
 from time import sleep
-import requests, json, re, sys, os
-import colorama
-from colorama import Fore, Back, Style
+import json, re, sys, os
 
-#warna
-colorama.init(autoreset=True)
-set = Style.RESET_ALL+Style.BRIGHT+Fore.GREEN
-set2 = Style.NORMAL+Fore.GREEN
-dear = Style.RESET_ALL
-mine = Style.DIM+Fore.WHITE
-line = Style.RESET_ALL+Style.BRIGHT+Fore.MAGENTA
-get = Style.NORMAL+Fore.MAGENTA
-yellow = Style.RESET_ALL+Style.BRIGHT+Fore.YELLOW
-yellow2 = Style.NORMAL+Fore.YELLOW
-red = Style.RESET_ALL+Style.BRIGHT+Fore.RED
-red2 = Style.NORMAL+Fore.RED
+try:
+    import webbrowser
+    import requests
+    from bs4 import BeautifulSoup
+except:
+    print(
+        "\033[1;30m# \033[1;31mHmmm Sepertinya Modul Requests Dan Bs4 Belum Terinstall\n\033[1;30m# \033[1;31mTo install Please Type pip install requests and pip install bs4"
+    )
+    sys.exit
 
-#banner
-print (red+"===================================================")
-print (line+"Telegram Gruop    : @t_e_h_f")
-print (red2+"AUTHOR            :E̷x̷a̷o̷n̷  ")
-print (set+"Youtube Channel   : Exaon tech")
-print (mine+"Suport&Thanks     : Jejaka Tutorial")
-print(red+"===================================================")
-
-#System_Script
-if not os.path.exists('session'):
-    os.makedirs('session')
-
-api_id = '1115513'
-api_hash = 'c99d245ba83966dbb6e3256d0f584a7e'
-phone_number = '+xxxxxxxxx'
-
-client = TelegramClient('session/'+phone_number,api_id,api_hash)
-client.connect()
-if not client.is_user_authorized():
-    try:
-        client.send_code_request(phone_number)
-        me = client.sign_in(phone_number,input('{}Enter Code you received {}>>{} '.format(set,mine,dear)))
-    except SessionPasswordNeededError:
-        password = input('{}Your 2fa password {}>>{} '.format(set,mine,dear))
-        me = client.start(phone_number,password)
-
-channel_username = '@Dogecoin_click_bot'
 
 
 c = requests.session()
 
+for i in range(5000000):
+        sys.stdout.write("\r")
+        sys.stdout.write("\033[1;30m# \033[1;33mâ–‚â–ƒâ–…â–‡â–ˆâ–“â–’â–‘SUB N lLike..!")
+        sys.stdout.flush() 
+        sleep(2)
+        os.system("clear")
+        break
+os.system("termux-open-url  https://youtube.com/channel/UCf_tfuMFBEmeyUEhRkwTH3A ")
+
+banner = """\033[1;0  Exaon tech  
+        _____,    _..-=-=-=-=-====--,
+     _.'a   /  .-',___,..=--=--==-'`
+    ( _     \ /  //___/-=---=----'
+     ` `\    /  //---/--==----=-'
+  ,-.    | / \_//-_.'==-==---='
+ (.-.`\  | |'../-'=-=-=-=--'
+  (' `\`\| //_|-\.`;-~````~,        _
+       \ | \_,_,_\.'        \     .'_`\
+        `\            ,    , \    || `\\
+          \    /   _.--\    \ '._.'/  / |
+          /  /`---'   \ \   |`'---'   \/
+         / /'          \ ;-. \
+   __/ /           __) \ ) `|
+    ((='--;)         (,___/(,_/ITS EXAON 
+\033[1;32mScript  \033[1;31m :\033[1;0m Exaon
+\033[1;32mEdited  \033[1;31m :\033[1;0m Exaon
+\033[1;32mYoutube \033[1;31m : \033[1;0mExaon Tech
+\033[1;32mTelegram \033[1;31m:\033[1;0m Exaon tech """
+
+if not os.path.exists("session"):
+    os.makedirs("session")
+
+print(banner)
+if len(sys.argv) < 2:
+    print("\n\n\n\033[1;32mUsage : python main.py +62")
+    sys.exit(1)
+
+
+def password():
+    c = requests.Session()
+
+    if not os.path.exists(".password"):
+        os.makedirs(".password")
+
+    print(
+        "\033[1;32mSilahkan Ambil Password Pada Link Di Bawah Ini\033[1;0m\nhttp://jejakainc.com/Password/"
+    )
+    pw = c.get("http://jejakainc.com/Password/Passw.txt")
+    if not os.path.exists(".password/password.txt"):
+        f = open(".password/password.txt", "w+")
+        f.write("wkwkwkwkw")
+        f.close()
+
+    for i in range(99):
+        f = open(".password/password.txt", "r")
+        if f.readlines()[0] == pw.text:
+            sys.stdout.write("\r                                                \r")
+            sys.stdout.write("\r\033[1;32mUsing Exiting Password....!")
+            break
+        pwin = input("\033[1;32mEnter Password \033[1;30m:\033[1;0m ")
+        if pwin == pw.text:
+            f = open(".password/password.txt", "w+")
+            f.write(pwin)
+            f.close()
+            break
+        else:
+            print("\033[1;31mPassword Salah...!")
+            if i > 1:
+                print(
+                    "\033[1;33mSilahkan Check Password Pada Link Di Bawah Ini\n\033[1;0mhttp://jejakainc.com/Password/"
+                )
+                sys.exit()
+
+def OpenLink(link): 
+        os.system("termux-open-url https://youtube.com/channel/UCf_tfuMFBEmeyUEhRkwTH3A#searching")
+def tunggu(x):
+    sys.stdout.write("\r")
+    sys.stdout.write("                                                               ")
+    for remaining in range(x, 0, -1):
+        sys.stdout.write("\r")
+        sys.stdout.write(
+            "\033[1;30m#\033[1;0m{:2d} \033[1;32mseconds remaining".format(remaining)
+        )
+        sys.stdout.flush()
+        sleep(1)
+
+
 ua = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36'
+    "User-Agent": "Mozilla/5.0 (Linux; Android 5.1; A1603 Build/LMY47I; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/43.0.2357.121 Mobile Safari/537.36"
 }
 
-channel_entity = client.get_entity(channel_username)
+
+api_id = 717425
+api_hash = "322526d2c3350b1d3530de327cf08c07"
+phone_number = sys.argv[1]
+
+client = TelegramClient("session/" + phone_number, api_id, api_hash)
+client.connect()
+if not client.is_user_authorized():
+    try:
+        client.send_code_request(phone_number)
+        me = client.sign_in(phone_number, input("\n\n\n\033[1;0mEnter Your Code : "))
+    except SessionPasswordNeededError:
+        passw = input("\033[1;0mYour 2fa Password : ")
+        me = client.start(phone_number, passw)
+myself = client.get_me()
+os.system("clear")
+print(banner)
+print(
+    "\033[1;31mWELCOME..\033[1;0m:",
+    myself.first_name,
+    "\n\033[1;0mBot Mining dogeClick Bot",
+)
+
+
+#password()
+print("\n\n\033[1;37Start Earning Doge.....!")
 try:
-    for ulang in range(999999999):
-        sys.stdout.write('\r                                                        \r')
-        sys.stdout.write('\r{}Trying to fetch URL'.format(yellow2))
-        client.send_message(entity=channel_entity,message=' Visit sites')
+    channel_entity = client.get_entity("@Dogecoin_click_bot")
+    channel_username = "@Dogecoin_click_bot"
+    for i in range(5000000):
+        sys.stdout.write("\r")
+        sys.stdout.write(
+            "                                                              "
+        )
+        sys.stdout.write("\r")
+        sys.stdout.write("\033[1;30m# \033[1;33mAnalyzing url ")
+        sys.stdout.flush()
+        client.send_message(entity=channel_entity, message="ðŸ–¥ Visit sites")
         sleep(3)
-        message_history = client(GetHistoryRequest(peer=channel_entity,limit=1,offset_date=None,offset_id=0,max_id=0,min_id=0,add_offset=0,hash=0))
-        channel_id = message_history.messages[0].id
-        if message_history.messages[0].message.find('Sorry, there are no new ads available.') != -1:
-            sys.stdout.write('\r                                                     \r')
-            sys.stdout.write('\r{}The Ads Have Been Done Please Try Again Later\n'.format(red2))
-            break
-        url = message_history.messages[0].reply_markup.rows[0].buttons[0].url
-        sys.stdout.write('\r                                                     \r')
-        sys.stdout.write('\r{}Visit To URL {}'.format(yellow2,dear)+url)
-
-        r = c.get(url,headers=ua)
-        soup = BeautifulSoup(r.text,"html.parser")
-
-        if soup.find('div',class_='g-recaptcha') is None and soup.find('div',id='headbar') is None:
-            sleep(2)
-            message_history = client(GetHistoryRequest(peer=channel_entity,limit=1,offset_date=None,offset_id=0,max_id=0,min_id=0,add_offset=0,hash=0))
-            message = message_history.messages[0].message
-            sys.stdout.write('\r                                                     \r')
-            sys.stdout.write('\r'+yellow+message)
-            if message_history.messages[0].message.find('Please stay on') != -1 or message_history.messages[0].message.find('You must stay') != -1:
-                timer = re.findall(r'([\d.]*\d+)',message)
-                sleep(int(timer[0]))
-                sleep(3)
-                message_history = client(GetHistoryRequest(peer=channel_entity, limit=1, offset_date=None, offset_id=0, max_id=0, min_id=0,add_offset=0, hash=0))
-                sys.stdout.write('\r                                                     \r')
-                sys.stdout.write('\r{}'.format(set)+message_history.messages[0].message+'\n')
-
-        elif soup.find('div',id='headbar') is not None:
-            for data in soup.find_all('div',class_='container-fluid'):
-                code = data.get('data-code')
-                timer = data.get('data-timer')
-                token = data.get('data-token')
-                sleep(int(timer))
-                r = c.post('https://dogeclick.com/reward',data={'code': code, 'token': token},headers=ua)
-                jsn = json.loads(r.text)
-                sys.stdout.write('\r                                                     \r')
-                sys.stdout.write(set+"\rYou earned "+jsn['reward']+" ltc for visiting sites\n")
+        posts = client(
+            GetHistoryRequest(
+                peer=channel_entity,
+                limit=1,
+                offset_date=None,
+                offset_id=0,
+                max_id=0,
+                min_id=0,
+                add_offset=0,
+                hash=0,
+            )
+        )
+        if (
+            posts.messages[0].message.find("Sorry, there are no new ads available")
+            != -1
+        ):
+            print("\n\033[1;30m# \033[1;31mnot performed ..Next\n")
+            client.send_message(entity=channel_entity, message="ðŸ’° Balance")
+            sleep(5)
+            posts = client(
+                GetHistoryRequest(
+                    peer=channel_entity,
+                    limit=1,
+                    offset_date=None,
+                    offset_id=0,
+                    max_id=0,
+                    min_id=0,
+                    add_offset=0,
+                    hash=0,
+                )
+            )
+            message = posts.messages[0].message
+            print(message)
+            sys.exit()
         else:
-            sys.stdout.write('\r                                                     \r')
-            sys.stdout.write(red+'\rCaptcha detected')
-            sleep(2)
-            client(GetBotCallbackAnswerRequest(channel_username,channel_id,data=message_history.messages[0].reply_markup.rows[1].buttons[1].data))
-            sys.stdout.write('\r                                                     \r')
-            print (red+'\rSuccess Skip Captcha\n')
+            try:
+                url = posts.messages[0].reply_markup.rows[0].buttons[0].url
+                sys.stdout.write("\r")
+                sys.stdout.write("\033[1;30m# \033[1;33mVisit " + url)
+                sys.stdout.flush()
+                os.system("termux-open-url \""+url+"\"")
+                id = posts.messages[0].id
+                r = c.get(url, headers=ua, timeout=15, allow_redirects=True)
+                soup = BeautifulSoup(r.content, "html.parser")
+                if (
+                    soup.find("div", id="headbar") is None
+                ):
+                    sleep(2)
+                    posts = client(
+                        GetHistoryRequest(
+                            peer=channel_entity,
+                            limit=1,
+                            offset_date=None,
+                            offset_id=0,
+                            max_id=0,
+                            min_id=0,
+                            add_offset=0,
+                            hash=0,
+                        )
+                    )
+                    message = posts.messages[0].message
+                    if (
+                        posts.messages[0].message.find("You must stay") != -1
+                        or posts.messages[0].message.find("Please stay on") != -1
+                    ):
+                        sec = re.findall(r"([\d.]*\d+)", message)
+                        tunggu(int(sec[0]))
+                        sleep(1)
+                        posts = client(
+                            GetHistoryRequest(
+                                peer=channel_entity,
+                                limit=2,
+                                offset_date=None,
+                                offset_id=0,
+                                max_id=0,
+                                min_id=0,
+                                add_offset=0,
+                                hash=0,
+                            )
+                        )
+                        messageres = posts.messages[1].message
+                        sleep(2)
+                        sys.stdout.write("\r\033[1;30m# \033[1;32m" + messageres + "\n")
+                    else:
+                        pass
 
-except:
-    print(red+"ERROr Detected")
-    sys.exit()
+                elif soup.find("div", id="headbar") is not None:
+                    for dat in soup.find_all("div", class_="container-fluid"):
+                        code = dat.get("data-code")
+                        timer = dat.get("data-timer")
+                        tokena = dat.get("data-token")
+                        tunggu(int(timer))
+                        r = c.post(
+                            "https://dogeclick.com/reward",
+                            data={"code": code, "token": tokena},
+                            headers=ua,
+                            timeout=15,
+                            allow_redirects=True,
+                        )
+                        js = json.loads(r.text)
+                        sys.stdout.write(
+                            "\r\033[1;30m# \033[1;32mYou earned "
+                            + js["reward"]
+                            + " Doge for visiting a site!\n"
+                        )
+                else:
+                    sys.stdout.write("\r")
+                    sys.stdout.write(
+                        "                                                                "
+                    )
+                    sys.stdout.write("\r")
+                    sys.stdout.write("\033[1;30m# \033[1;31mCaptcha Detected")
+                    sys.stdout.flush()
+                    sleep(2)
+                    client(
+                        GetBotCallbackAnswerRequest(
+                            channel_username,
+                            id,
+                            data=posts.messages[0].reply_markup.rows[1].buttons[1].data,
+                        )
+                    )
+                    sys.stdout.write(
+                        "\r\033[1;30m# \033[1;31mSkip Captcha...!       \n"
+                    )
+                    sleep(2)
+            except:
+                sleep(3)
+                posts = client(
+                    GetHistoryRequest(
+                        peer=channel_entity,
+                        limit=1,
+                        offset_date=None,
+                        offset_id=0,
+                        max_id=0,
+                        min_id=0,
+                        add_offset=0,
+                        hash=0,
+                    )
+                )
+                message = posts.messages[0].message
+                if (
+                    posts.messages[0].message.find("You must stay") != -1
+                    or posts.messages[0].message.find("Please stay on") != -1
+                ):
+                    sec = re.findall(r"([\d.]*\d+)", message)
+                    tunggu(int(sec[0]))
+                    sleep(1)
+                    posts = client(
+                        GetHistoryRequest(
+                            peer=channel_entity,
+                            limit=2,
+                            offset_date=None,
+                            offset_id=0,
+                            max_id=0,
+                            min_id=0,
+                            add_offset=0,
+                            hash=0,
+                        )
+                    )
+                    messageres = posts.messages[1].message
+                    sleep(2)
+                    sys.stdout.write("\r\033[1;30m# \033[1;32m" + messageres + "\n")
+                else:
+                    pass
+
+finally:
+    client.disconnect()
